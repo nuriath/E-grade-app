600c150aa305cf1a8a767bb11f713ade934b84eb
Mwangaza Nuriath <mnuriath28@gmail.com>
2019-01-15 11:31:17 +0200

    add a link in html files

---
+++ js/register.js
@@ -0,0 +1,49 @@
+$(document).ready(function() {
+    $("#register").click(function() {
+    var name = $("#name").val();
+    var email = $("#email").val();
+    var password = $("#password").val();
+    var cpassword = $("#cpassword").val();
+    if (name == '' || email == '' || password == '' || cpassword == '') {
+    alert("Please fill all fields...!!!!!!");
+    } else if ((password.length) < 8) {
+    alert("Password should atleast 8 character in length...!!!!!!");
+    } else if (!(password).match(cpassword)) {
+    alert("Your passwords don't match. Try again?");
+    } else {
+    $.post("register.php", {
+    name1: name,
+    email1: email,
+    password1: password
+    }, function(data) {
+    if (data == 'You have Successfully Registered.....') {
+    $("form")[0].reset();
+
+    //data base
+  /*  <?php
+$connection = mysql_connect("localhost", "root", ""); // Establishing connection with server..
+$db = mysql_select_db("college", $connection); // Selecting Database.
+$name=$_POST['name1']; // Fetching Values from URL.
+$email=$_POST['email1'];
+$password= sha1($_POST['password1']); // Password Encryption, If you like you can also leave sha1.
+// Check if e-mail address syntax is valid or not
+$email = filter_var($email, FILTER_SANITIZE_EMAIL); // Sanitizing email(Remove unexpected symbol like <,>,?,#,!, etc.)
+if (!filter_var($email, FILTER_VALIDATE_EMAIL)){
+echo "Invalid Email.......";
+}else{
+$result = mysql_query("SELECT * FROM registration WHERE email='$email'");
+$data = mysql_num_rows($result);
+if(($data)==0){
+$query = mysql_query("insert into registration(name, email, password) values ('$name', '$email', '$password')"); // Insert query
+if($query){
+echo "You have Successfully Registered.....";
+}else
+{
+echo "Error....!!";
+}
+}else{
+echo "This email is already registered, Please try another email...";
+}
+}
+mysql_close ($connection);
+?>*/
