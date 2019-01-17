9965e52d3dc28640ed50ae9c198dd16721ce8100
Mwangaza Nuriath <mnuriath28@gmail.com>
2019-01-17 18:15:24 +0200

    i put styles in past winner miss file

--- js/question.js
+++ js/question.js
@@ -1,24 +1,24 @@
-var lakes = ["Kivu","Victoria","Tanganyika","Malawi"];
-var countries = ["Uganda","Rwanda","Tanzaniya","Malawi"];
-var random = Math.floor(Math.random() * lakes.length);
+var questions = ["which country have lake Victoria","Entomology is the science that studies","Hitler party which came into power in 1933 is known as", "In which year of First World War Germany declared war on Russia and France?", "With which sport is the Jules Rimet trophy associated?"];
+var answers = ["Tanzaniya", "Insects", "Nazi party", "1914","Football"];
+var random = Math.floor(Math.random() * questions.length);
 
-  document.getElementById("name").innerHTML = lakes[random];
+  document.getElementById("name").innerHTML = questions[random];
 
 function submit() {
-    var sub = input.value;
-    if (random == 0 && sub == countries[1]) {
+    var sub = document.getElementById('input').value;
+    if (random == 0 && sub == answers[0]) {
         document.getElementById("answer").innerHTML = "Correct";
         document.getElementById("button").innerHTML = "<button onclick=btn()>Next</button>";
     } 
-    else if (random == 1 && countries[0]) {
+    else if (random == 1 && answers[1]) {
         document.getElementById("answer").innerHTML = "Correct";
         document.getElementById("button").innerHTML = "<button onclick=btn()>Next</button>";
     } 
-    else if (random == 2 && countries[2]) {
+    else if (random == 2 && answers[2]) {
         document.getElementById("answer").innerHTML = "Correct";
         document.getElementById("button").innerHTML = "<button onclick=btn()>Next</button>";
     }
-    else if (random == 3 && countries[3]) {
+    else if (random == 3 && answers[3]) {
         document.getElementById("answer").innerHTML = "Correct";
         document.getElementById("button").innerHTML = "<button onclick=btn()>Next</button>";
     }
@@ -29,8 +29,8 @@ function submit() {
   } 
 
   function btn() {
-    random = Math.floor(Math.random() * lakes.length);
-    document.getElementById("name").innerHTML = lakes[random];
+    random = Math.floor(Math.random() * questions.length);
+    document.getElementById("name").innerHTML = questions[random];
     document.getElementById("button").innerHTML = "";
     document.getElementById("answer").innerHTML = "";
   }
